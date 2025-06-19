3ce1681fd938d4de927d1824cb43390a1d71acd7
Nick <174647283+npkav@users.noreply.github.com>
2025-06-19 19:29:32 -0230

    Service rep entity

---
+++ src/main/java/com/keyin/server/entity/ServiceRep.java
@@ -0,0 +1,35 @@
+package com.keyin.server.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "service_reps")
+public class ServiceRep {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    private String email;
+    
+    
+    
+    public ServiceRep() {}
+    
+    public Long getID() { return id; }
+    public void setID(Long id) { this.id = id; }
+    
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+} 
