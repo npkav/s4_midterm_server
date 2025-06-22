cef8c9290f54924aae3ee3a15765981f6d121137
EvKavK <evan.kavanagh@keyin.com>
2025-06-21 23:52:25 -0230

    Added necessary repositories for CRUD implementation.

---
+++ src/main/java/com/keyin/server/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.keyin.server.repository;
+
+import com.keyin.server.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    
+}
---
+++ src/main/java/com/keyin/server/repository/ServiceRepRepository.java
@@ -0,0 +1,10 @@
+package com.keyin.server.repository;
+
+import com.keyin.server.entity.ServiceRep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepRepository extends JpaRepository<ServiceRep, Long> {
+    
+}
