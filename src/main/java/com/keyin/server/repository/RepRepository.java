8021d8d3ca1921063e79ae8c6e6a2f1a6e713eb0
EvKavK <evan.kavanagh@keyin.com>
2025-06-23 16:03:13 -0230

    Added functionality for viewing all reps under a specific department.

--- src/main/java/com/keyin/server/repository/RepRepository.java
+++ src/main/java/com/keyin/server/repository/RepRepository.java
@@ -3,8 +3,9 @@ package com.keyin.server.repository;
 import com.keyin.server.entity.ServiceRep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface RepRepository extends JpaRepository<ServiceRep, Long> {
-    
+    List<ServiceRep> findByDepartmentID(Long departmentId);
 }
