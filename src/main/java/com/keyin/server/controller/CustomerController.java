76d35963285d62994d14764ec81e56dc9fe98984
EvKavK <evan.kavanagh@keyin.com>
2025-06-24 13:37:47 -0230

    Adjusted CustomerController mapping.

--- src/main/java/com/keyin/server/controller/CustomerController.java
+++ src/main/java/com/keyin/server/controller/CustomerController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/customer")
 public class CustomerController {
 
     @Autowired
